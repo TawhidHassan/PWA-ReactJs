26d0a7c935841c6ba0bb9b61f228c661b077e8aa
sifat007 <sifat27.ht@gmail.com>
2021-02-03 16:49:24 +0600

    install router and make componnent and connect with router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar,Button,Form,Nav,FormControl } from "react-bootstrap";
-
+import { Navbar, Button, Form, Nav, FormControl  } from "react-bootstrap";
+import { Link, Route, BrowserRouter as Router,Switch } from 'react-router-dom';
+import Home from './Home'
+import About from './About'
+import Users from './Users'
 function App() {
   return (
     <div className="App">
+       <Router>
        <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-light">Search</Button>
-    </Form>
-  </Navbar>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+        <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link ><Link to="/about" >About</Link></Nav.Link>
+            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
+
+      <Switch>
+          <Route path="/about" component={About} ></Route>
+          <Route path="/users" component={Users} ></Route>
+          <Route path="/" component={Home} ></Route>
+        </Switch>
+
+       </Router>
+      
     </div>
   );
 }
